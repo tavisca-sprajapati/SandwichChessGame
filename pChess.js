561456212f83c5dc6380af134f6a1afc4e682cbd
parulgupta12 <pgupta@tavisca.com>
2014-08-30 20:25:50 +0530

    .js file for save function

---
+++ pChess.js
@@ -0,0 +1,8 @@
+function() save(){
+var playerName=prompt("Enter the name of the player:", "Parul Gupta");
+if(playerName!=null)
+{
+	document.write("Hello");
+	innerHTML="Your game" +playerName+ "is saved";
+	}
+}
