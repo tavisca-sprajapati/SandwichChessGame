ee523e3af4812cd456b1899cf98055660e153da6
parulgupta12 <pgupta@tavisca.com>
2014-08-31 17:26:09 +0530

    Update pChess.js

--- pChess.js
+++ pChess.js
@@ -1,8 +1,25 @@
-function() save(){
+function save1()
+{
+alert('hi');
+var list =[];
 var playerName=prompt("Enter the name of the player:", "Parul Gupta");
+
+
 if(playerName!=null)
 {
-	document.write("Hello");
-	innerHTML="Your game" +playerName+ "is saved";
+	//document.getElementById("savebtn");
+	//document.write("Hello");
+	innerHTML="Your game" + playerName + ", is saved";
+	
+	for(var i=0; i<8;i++)
+	{
+	for(var j=0;j<8;j++)
+    {
+	var ids=""+i+j;
+	alert(ids);
+	var str=document.getElementById(sids).innerHTML;
+	alert(id);
 	}
-}
+	}
+	}
+	}
