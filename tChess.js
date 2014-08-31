3c14063e73db95e27d04497414e702cc4753a05b
tkatariya <tkatariya@tavisca.com>
2014-08-31 17:31:02 +0530

    replay function with 3 seconds time delay

--- tChess.js
+++ tChess.js
@@ -1,26 +1,26 @@
 
-var arr = ["10","30","11","31","12","32","60","40","61","41","62","42"];
-	
+var arr = ["10-30","11-31","12-32","60-40","61-41","62-42"];
+
+var replay = function(i) {
+   var s = document.getElementById("div1"); //you could pass this element as a parameter as well
+   i = i || 0;
+   if(i < 6) {
+      str2=""+arr[i];
+
+		var res1 = str2.substring(0,2);
+		var res2 = str2.substring(3,5);
+
+			var xyx=document.getElementById(res1).innerHTML;
+			document.getElementById(res2).innerHTML=xyx;
+			document.getElementById(res1).innerHTML="";
+
+      i++;
+      //create a pause of 2 seconds.
+      setTimeout(function() { replay(i) }, 3000);          
+   }
+}
 
 
 
-var myVar=setInterval(function () {replay()}, 1000);
-
-function replay(){
-	
-		for( i=0; i<arr.length; i+2)
-		{
-		
-			var abc=document.getElementById(arr[i]).innerHTML;
-			
-	
-			//document.getElementById(arr[i+1]).innerHTML="";
-
-			document.getElementById(arr[i+1]).innerHTML=abc;	
-			document.getElementById(arr[i]).innerHTML="";	
-		
-		
-		}
-	}
 
 
