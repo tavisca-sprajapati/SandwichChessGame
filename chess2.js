61325f8b1f54340811f56b2995db4bad79b5fc64
shahbazkhan003 <skhan@tavisca.com>
2014-09-01 22:25:28 +0530

    final game

--- chess2.js
+++ chess2.js
@@ -12,28 +12,8 @@ wNight="<img src='images/wN.png'>";
  wQueen="<img src='images/wQ.png'>";
  wRook="<img src='images/wR.png'>";
 wPawn="<img src='images/wP.png'>";
-
-
-/*
-var bBoardList = [];
-var wBoardList=[];
-		bBoardList.push({id:1,peace:'bRook'});	
-        bBoardList.push({id:2,peace:'bNight'});
-		bBoardList.push({id:3,peace:'bBishop'});
-		bBoardList.push({id:4,peace:'bQueen'});
-		bBoardList.push({id:5,peace:'bKing'});
-		bBoardList.push({id:6,peace:'bPawn'});
 		
-		wBoardList.push({id:1,peace:'wRook'});	
-		wBoardList.push({id:2,peace:'wNight'});
-		wBoardList.push({id:3,peace:'wBishop'});
-		wBoardList.push({id:4,peace:'wQueen'});
-		wBoardList.push({id:5,peace:'wKing'});
-		wBoardList.push({id:6,peace:'wPawn'});*/
-		
-
-		
-window.onload=function()
+function makeBoard()
 {
 //dispaly all pawn
 for (i = 1; i < 7 ;)
@@ -46,74 +26,65 @@ for (i = 1; i < 7 ;)
 		 myimage=bPawn;
 		 if(i==6)
 		 myimage=wPawn;
-		document.getElementById(str).innerHTML=myimage;
-		document.getElementById(str).setAttribute("style","cursor:pointer;");
-		document.getElementById(str).setAttribute("onclick","play(this)");
+		tempObjects=document.getElementById(str);
+		tempObjects.innerHTML=myimage;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 	}
 i=i+5;	
 
 
 }
-/*
-   a=3;
-   for(i=0;i<8;i++)
-   {	
-     
-	str1=""+0+i;
-        if(i<=4)
-        documnet.getElementById("00").innerHTML=bBoardList[i+1].peace;
-		else
-		{
-		documnet.getElementById("11").innerHTML=bBoardList[i-a].peace;
-		 a++;
-		}
-		
-		document.getElementById(str).setAttribute("style","cursor:pointer;");
-		document.getElementById(str).setAttribute("onclick","play(this)");
-		}
-		*/
-		
-		//black peace
-		document.getElementById("00").innerHTML=bRook;
-		document.getElementById("00").setAttribute("style","cursor:pointer;");
-		document.getElementById("00").setAttribute("onclick","play(this)");
+
+	 tempObjects=document.getElementById("00");
+		tempObjects.innerHTML=bRook;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
-         document.getElementById("01").innerHTML=bNight;
-		document.getElementById("01").setAttribute("style","cursor:pointer;");
-		document.getElementById("01").setAttribute("onclick","play(this)");
+		tempObjects=document.getElementById("01");
+        tempObjects.innerHTML=bNight;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
-		  document.getElementById("02").innerHTML=bBishop;
-		document.getElementById("02").setAttribute("style","cursor:pointer;");
-		document.getElementById("02").setAttribute("onclick","play(this)");
+		tempObjects=document.getElementById("02");
+		  tempObjects.innerHTML=bBishop;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
-		  document.getElementById("03").innerHTML=bQueen;
-		document.getElementById("03").setAttribute("style","cursor:pointer;");
-		document.getElementById("03").setAttribute("onclick","play(this)");
+
+		 tempObjects =document.getElementById("03");
+		 tempObjects.innerHTML=bQueen;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
-		  document.getElementById("04").innerHTML=bKing;
-		document.getElementById("04").setAttribute("style","cursor:pointer;");
-		document.getElementById("04").setAttribute("onclick","play(this)");
+		 tempObjects= document.getElementById("04");
+		 tempObjects.innerHTML=bKing;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
-		 document.getElementById("05").innerHTML=bBishop;
-		document.getElementById("05").setAttribute("style","cursor:pointer;");
-		document.getElementById("05").setAttribute("onclick","play(this)");
+		tempObjects= document.getElementById("05");
+		tempObjects.innerHTML=bBishop;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
 		
 				
-         document.getElementById("06").innerHTML=bNight;
-		document.getElementById("06").setAttribute("style","cursor:pointer;");
-		document.getElementById("06").setAttribute("onclick","play(this)");
+        tempObjects= document.getElementById("06");
+        tempObjects.innerHTML=bNight;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
-		document.getElementById("07").innerHTML=bRook;
-		document.getElementById("07").setAttribute("style","cursor:pointer;");
-		document.getElementById("07").setAttribute("onclick","play(this)");
+		tempObjects=document.getElementById("07");
+		tempObjects.innerHTML=bRook;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
 		
@@ -122,44 +93,52 @@ i=i+5;
 		//WHITE PEACE
 		
 		
-		document.getElementById("70").innerHTML=wRook;
-		document.getElementById("70").setAttribute("style","cursor:pointer;");
-		document.getElementById("70").setAttribute("onclick","play(this)");
+		tempObjects=document.getElementById("70");
+		tempObjects.innerHTML=wRook;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
-         document.getElementById("71").innerHTML=wNight;
-		document.getElementById("71").setAttribute("style","cursor:pointer;");
-		document.getElementById("71").setAttribute("onclick","play(this)");
+        tempObjects= document.getElementById("71");
+        tempObjects.innerHTML=wNight;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 
-		  document.getElementById("72").innerHTML=wBishop;
-		document.getElementById("72").setAttribute("style","cursor:pointer;");
-		document.getElementById("72").setAttribute("onclick","play(this)");
+		 tempObjects= document.getElementById("72");
+		 tempObjects.innerHTML=wBishop;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
-		  document.getElementById("73").innerHTML=wQueen;
-		document.getElementById("73").setAttribute("style","cursor:pointer;");
-		document.getElementById("73").setAttribute("onclick","play(this)");
+		 tempObjects= document.getElementById("73");
+		 tempObjects.innerHTML=wQueen;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
-		  document.getElementById("74").innerHTML=wKing;
-		document.getElementById("74").setAttribute("style","cursor:pointer;");
-		document.getElementById("74").setAttribute("onclick","play(this)");
+		 tempObjects = document.getElementById("74");
+		 tempObjects.innerHTML=wKing;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
-		 document.getElementById("75").innerHTML=wBishop;
-		document.getElementById("75").setAttribute("style","cursor:pointer;");
-		document.getElementById("75").setAttribute("onclick","play(this)");
+		tempObjects= document.getElementById("75");
+		tempObjects.innerHTML=wBishop;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 	
-         document.getElementById("76").innerHTML=wNight;
-		document.getElementById("76").setAttribute("style","cursor:pointer;");
-		document.getElementById("76").setAttribute("onclick","play(this)");
+        tempObjects= document.getElementById("76");
+        tempObjects.innerHTML=wNight;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 		
-		document.getElementById("77").innerHTML=wRook;
-		document.getElementById("77").setAttribute("style","cursor:pointer;");
-		document.getElementById("77").setAttribute("onclick","play(this)");
+		tempObjects=document.getElementById("77");
+		tempObjects.innerHTML=wRook;
+		tempObjects.setAttribute("style","cursor:pointer;");
+		tempObjects.setAttribute("onclick","play(this)");
 		
 			
 }
